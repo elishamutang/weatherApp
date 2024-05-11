6355cbde905e51906a51e8c14d83c06f1d080dd0
elishamutang <elishamutang@outlook.com>
2024-05-12 01:27:57 +0800

    index file done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import styles from './styles.css'
+import cloudy from '../asset/noaa-99F4mC79j1I-unsplash.jpg'
+import displayWeather from './getWeather'
+
+const bodyElem = document.querySelector('body')
+bodyElem.style.backgroundImage = `url(${cloudy})`
+
+displayWeather()
