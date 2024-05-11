cdb070e2eee367f3dd6383e22cd624906242e5b2
elishamutang <elishamutang@outlook.com>
2024-05-11 21:55:02 +0800

    initial stage.

--- webpack.common.js
+++ webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '',
+            title: "What's the weather?",
             filename: 'index.html',
             template: 'src/template.html',
         }),
