95cf7fc9992334298602179873cdcda3662623d2
elishamutang <elishamutang@outlook.com>
2024-05-15 00:17:21 +0800

    [refactor] Add background image dynamically.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import styles from './styles.css'
+import background from '../asset/noaa-99F4mC79j1I-unsplash.jpg'
 import displayWeather from './getWeather'
+
+const backgroundImg = document.createElement('img')
+backgroundImg.id = 'backgroundImg'
+backgroundImg.src = background
+
+const body = document.querySelector('body')
+
+body.insertAdjacentElement('afterbegin', backgroundImg)
 
 displayWeather()
