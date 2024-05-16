64142be38b9298eba3a133055132ed422da339c3
elishamutang <elishamutang@outlook.com>
2024-05-16 23:47:01 +0800

    [update] Added commentary.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import styles from './styles.css'
 import background from '../asset/noaa-99F4mC79j1I-unsplash.jpg'
 import displayWeather from './getWeather'
 
+// Set background image.
 const backgroundImg = document.createElement('img')
 backgroundImg.id = 'backgroundImg'
 backgroundImg.src = background
